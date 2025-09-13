9a1877f43512447dde672dec325b9f19f0954254
Tomaz Zlindra <tomz0840@gmail.com>
2025-09-13 14:00:53 -0700

    all files added

---
+++ Core/Inc/test.h
@@ -0,0 +1,22 @@
+/*
+ * test.hpp
+ *
+ *
+ */
+
+#ifndef INC_TEST_H_
+#define INC_TEST_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void cpp_main();  // C-callable wrapper
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
+#endif /* INC_TEST_H_ */
---
+++ Core/Src/test.cpp
@@ -0,0 +1,12 @@
+/*
+ * test.cpp
+
+ */
+
+#include "../Inc/test.h"
+
+void cpp_main() {
+	int x = 1;
+	x += 2;
+}
+
