f3d685be469c27af892a4bfb5fc359a210c125a7
Tomaz Zlindra <tomz0840@gmail.com>
2025-09-27 22:05:47 -0700

    Lumex LCD: renormalize, skeleton partially done

---
+++ Core/Inc/LCD/LumexLCD.h
@@ -0,0 +1,19 @@
+#ifndef INC_LCD_LUMEXLCD_H_
+#define INC_LCD_LUMEXLCD_H_
+
+#include "main.h"
+#include "string.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void lumex_lcd_main(TIM_HandleTypeDef* timer);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
+#endif /* INC_LCD_LUMEXLCD_H_ */
