0cdf4c951279a56cb64a9cb0d33279a8519b2975
Farshad <fmislam.3105@gmail.com>
2025-11-25 19:03:13 -0700

    Implemented CDC_Transmit_FS for USB device peripheral without compiler errors, ready for pull request

--- Core/Inc/Tasks/USB/USBController.hpp
+++ Core/Inc/Tasks/USB/USBController.hpp
@@ -11,10 +11,16 @@ class USBController
 
         bool Init();
         void Run();
+        void SafeIncrement(size_t);
     private:
         CircularBufferReader<optical_encoder_output_data> _buffer_reader_oe;
         CircularBufferReader<forcesensor_output_data> _buffer_reader_fs;
         CircularBufferReader<bpm_output_data> _buffer_reader_bpm;
 
+        optical_encoder_output_data oe_output; // Pointers to each of these structs
+        forcesensor_output_data fs_output;
+        bpm_output_data bpm_output;
+
         uint8_t _txBuffer[USB_CDC_TX_BUFFER_SIZE];
-}
+        int _txBufferIndex = 0;
+};
--- Core/Src/Tasks/USB/USBController.cpp
+++ Core/Src/Tasks/USB/USBController.cpp
@@ -10,48 +10,39 @@ bool USBController::Init()
 
 void USBController::Run()
 {
-	optical_encoder_output_data oe_output;
-	forcesensor_output_data fs_output;
-	bpm_output_data bpm_output;
-	
+
 	while(1)
 	{
-		_buffer_reader_oe.GetElementAndIncrementIndex(oe_output);
-		_buffer_reader_fs.GetElementAndIncrementIndex(fs_output);
-		_buffer_reader_bpm.GetElementAndIncrementIndex(bpm_output);
+		if (_buffer_reader_oe.GetElementAndIncrementIndex(oe_output)) { // Takes in struct but only passes in address
+			memcpy(_txBuffer + _txBufferIndex, &oe_output, sizeof(oe_output));
+			USBController::SafeIncrement(sizeof(oe_output));
+		}
+		
+		if (_buffer_reader_fs.GetElementAndIncrementIndex(fs_output)) {
+			memcpy(_txBuffer + _txBufferIndex, &fs_output, sizeof(fs_output));
+			USBController::SafeIncrement(sizeof(fs_output));
+		}
+
+		if (_buffer_reader_bpm.GetElementAndIncrementIndex(bpm_output)) {
+			memcpy(_txBuffer + _txBufferIndex, &bpm_output, sizeof(bpm_output));
+			USBController::SafeIncrement(sizeof(bpm_output));
+		}
 		osDelay(1);
 	}
 
 }
 
-extern "C" void usb_main()
-{
-	USBController usb = USBController();
-
-	if (!usb.Init())
-	{
-		return;
+void USBController::SafeIncrement(size_t addSize) {
+	if (_txBufferIndex + sizeof(oe_output) >= USB_CDC_TX_BUFFER_SIZE) {
+		// Transmit to USB when safe
+		while (CDC_Transmit_FS(_txBuffer, _txBufferIndex) == USBD_BUSY) {
+			osDelay(1);
+		}
+		_txBufferIndex = 0;
+	} else {
+		_txBufferIndex = _txBufferIndex + sizeof(oe_output);
+		// Must progress through txBuffer to account for its indices' size being smaller than that of each struct it contains
 	}
-
-	usb.Run();
-}
-#include <Tasks/USB/usb_main.h>
-#include <Tasks/USB/USBController.hpp>
-
-#include <CircularBufferReader.hpp>
-
-bool USBController::Init()
-{
-	return true;
-}
-
-void USBController::Run()
-{
-	while(1)
-	{
-
-	}
-
 }
 
 extern "C" void usb_main()
