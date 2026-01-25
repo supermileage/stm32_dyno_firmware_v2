c6ee0fd75faf0974ade901455d968f24a1748c77
Tomaz Zlindra <111657420+TZlindra@users.noreply.github.com>
2026-01-24 16:05:03 -0800

    USB Controller: Receive Data Fix, now copies into a seperate buffer (#91)

--- USB_DEVICE/App/usbd_cdc_if.c
+++ USB_DEVICE/App/usbd_cdc_if.c
@@ -94,7 +94,8 @@ uint8_t UserRxBufferFS[APP_RX_DATA_SIZE];
 uint8_t UserTxBufferFS[APP_TX_DATA_SIZE];
 
 /* USER CODE BEGIN PRIVATE_VARIABLES */
-
+uint8_t usb_controller_rx_buffer[USB_CONTROLLER_RX_BUFFER_SIZE];
+size_t usb_controller_rx_index = 0;
 /* USER CODE END PRIVATE_VARIABLES */
 
 /**
@@ -260,12 +261,38 @@ static int8_t CDC_Control_FS(uint8_t cmd, uint8_t* pbuf, uint16_t length)
   */
 static int8_t CDC_Receive_FS(uint8_t* Buf, uint32_t *Len)
 {
+  
   /* USER CODE BEGIN 6 */
+
+  uint8_t status = USBD_OK;
+  if (*Len > USB_CONTROLLER_RX_BUFFER_SIZE) 
+  {
+    // drop packet or set error flag
+    status = USBD_FAIL;
+    goto complete_transfer;
+  }
+
+  uint32_t len = *Len;
+  uint32_t idx = usb_controller_rx_index;
+
+  uint32_t space_to_end = USB_CONTROLLER_RX_BUFFER_SIZE - idx;
+
+  if (len <= space_to_end)
+  {
+    memcpy(&usb_controller_rx_buffer[idx], Buf, len);
+  }
+  else
+  {
+    memcpy(&usb_controller_rx_buffer[idx], Buf, space_to_end);
+    memcpy(&usb_controller_rx_buffer[0], Buf + space_to_end, len - space_to_end);
+  }
+
+  usb_controller_rx_index = (idx + len) % USB_CONTROLLER_RX_BUFFER_SIZE;
+
+complete_transfer:
   USBD_CDC_SetRxBuffer(&hUsbDeviceFS, &Buf[0]);
   USBD_CDC_ReceivePacket(&hUsbDeviceFS);
-  CDC_Transmit_FS(UserRxBufferFS, *Len);
-  memset(UserRxBufferFS, '\0', *Len);
-  return (USBD_OK);
+  return (status); 
   /* USER CODE END 6 */
 }
 
